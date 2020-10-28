c58661cd5a564300fbef418c48df17479a193b36
Ravi Kumar Gupta <42373914+rkg4037@users.noreply.github.com>
2020-10-28 19:59:57 +0530

    Apis implemented

--- main/java/com/group21/project/ProjectApplication.java
+++ main/java/com/group21/project/ProjectApplication.java
@@ -3,6 +3,7 @@ package com.group21.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ProjectApplication {
 
