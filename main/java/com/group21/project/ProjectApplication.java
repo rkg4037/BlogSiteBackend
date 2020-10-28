3f92ddc0ddf7b1ddbbaa1534c66c4a3d94e04359
Ravi Kumar Gupta <42373914+rkg4037@users.noreply.github.com>
2020-10-28 20:01:08 +0530

    Apis implemented , recommendedBlogs yet to be implemented

--- main/java/com/group21/project/ProjectApplication.java
+++ main/java/com/group21/project/ProjectApplication.java
@@ -3,7 +3,6 @@ package com.group21.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class ProjectApplication {
 
